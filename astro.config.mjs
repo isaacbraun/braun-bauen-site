77fafb225f00f58e4dda0d8c9fe94914a263ffbc
Isaac Braun <isaacbraun2000@gmail.com>
2025-05-06 14:35:02 -0700

    feat: enable responsiveImages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import alpinejs from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://braunbauen.com',
+  site: "https://braunbauen.com",
   integrations: [mdx(), sitemap(), alpinejs()],
 
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  experimental: {
+    responsiveImages: true,
   },
 });
