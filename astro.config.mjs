d167bcc8848b39818ff20503bcacb7575fc61643
Isaac Braun <isaacbraun2000@gmail.com>
2025-05-06 01:31:27 -0700

    feat: initial rewrite, homepage/cover letter not done

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwindcss from '@tailwindcss/vite';
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  site: 'https://braunbauen.com',
+  integrations: [mdx(), sitemap(), alpinejs()],
 
   vite: {
     plugins: [tailwindcss()],
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Braun Bauen';
+export const SITE_DESCRIPTION = 'Braun Bauen is a creative agency founded by software engineer and creative Isaac Braun. Our mission is to build products and experiences that are fast, accessible, and intuitive to use. Contact us at contact@braunbauen.com.';
